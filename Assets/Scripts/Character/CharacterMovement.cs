7bbf4b81970a2d379113471db9f3117c85f93f8a
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2019-01-25 22:55:28 +0100

    Inicio del movimiento del personaje

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed = 6;
+
+    private void Update() {
+        // Comprobamos si estamos intentando desplazarnos
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if (horizontalMovement != 0) {
+            transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalMovement);
+        }
+
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+        if (verticalMovement != 0) {
+            transform.Translate(Vector3.up * speed * Time.deltaTime * verticalMovement);
+        }
+    }
+}
