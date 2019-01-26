492929d9995c8e71814e0c709a1acbf46e617197
erik <errues@gmail.com>
2019-01-26 02:00:23 +0100

    Principios de habitación

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+    public SpriteRenderer lightRoomSprite;
+
+    public float fadingSpeed = 1;
+
+    private bool enlighted;
+    private bool fading;
+
+    private float alpha;
+
+    private void Awake() {
+        enlighted = false;
+        fading = false;
+
+        alpha = 0;
+
+        lightRoomSprite.color = new Color(1, 1, 1, 0);
+    }
+
+    private void Update() {
+        if (fading) {
+            alpha = Mathf.Clamp(alpha + fadingSpeed * Time.deltaTime, 0, 1);
+            lightRoomSprite.color = new Color(1, 1, 1, alpha);
+
+            if (alpha == 1) {
+                fading = false;
+            }
+        }
+    }
+
+    private void Enlighten() {
+        enlighted = true;
+        fading = true;
+    }
+}
