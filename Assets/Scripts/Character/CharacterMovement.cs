17e57eb4e6eb30900ff1efb845b6b125687053a9
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2019-01-26 17:54:40 +0100

    Movimiento dirigido del personaje

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -4,29 +4,70 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-    public float speed = 6;
+    [Header("Character Speed")]
+    public float movementSpeed = 6;
+    public float drivenMovementSpeed = 10;
+
+    [Header("Character Driven Path")]
+    public List<Vector2> drivenPath = new List<Vector2>();
 
     private bool drivenMovemen = false;
     private Rigidbody2D r2D;
 
+    private Vector3 initialMovementPosition;
+    private Vector3 destMovementPosition;
+    private float normalizedDrivenMovementSpeed;
+    private float lerpMovementStep;
+
+    public bool EnabledInteraction { get; set; }
+
     private void Awake() {
         r2D = GetComponent<Rigidbody2D>();
+        if(drivenPath.Count > 0) {
+            DoNextDrivenMovement();
+        }
+    }
+
+    public void DrivenMovement(List<Vector2> drivenPath) {
+        this.drivenPath = drivenPath;
+        DoNextDrivenMovement();
+    }
+
+    private void DoNextDrivenMovement() {
+        if (drivenPath.Count > 0) {
+            drivenMovemen = true;
+            initialMovementPosition = transform.position;
+            destMovementPosition = new Vector3(drivenPath[0].x, drivenPath[0].y, transform.position.z);
+            normalizedDrivenMovementSpeed = drivenMovementSpeed / Vector3.Distance(initialMovementPosition, destMovementPosition);
+            lerpMovementStep = 0f;
+            drivenPath.RemoveAt(0);
+        } else {
+            drivenMovemen = false;
+        }
     }
 
     private void FixedUpdate() {
-        if (!drivenMovemen) {
+        if (!drivenMovemen && EnabledInteraction) {
             // Comprobamos si estamos intentando desplazarnos
             float horizontalMovement = Input.GetAxisRaw("Horizontal");
             if (horizontalMovement != 0) {
-                transform.Translate(Vector3.right * speed * Time.fixedDeltaTime * horizontalMovement);
+                transform.Translate(Vector3.right * movementSpeed * Time.fixedDeltaTime * horizontalMovement);
             }
 
             float verticalMovement = Input.GetAxisRaw("Vertical");
             if (verticalMovement != 0) {
-                transform.Translate(Vector3.up * speed * Time.fixedDeltaTime * verticalMovement);
+                transform.Translate(Vector3.up * movementSpeed * Time.fixedDeltaTime * verticalMovement);
             }
 
             r2D.MovePosition(transform.position);
+        } else {
+            if (lerpMovementStep <= 1f) {
+                lerpMovementStep += normalizedDrivenMovementSpeed * Time.fixedDeltaTime;
+                transform.position = Vector3.Lerp(initialMovementPosition, destMovementPosition, lerpMovementStep);
+            } else {
+                transform.position = destMovementPosition;
+                DoNextDrivenMovement();
+            }
         }
     }
 
--- Assets/Scripts/Controllers/MapController.cs
+++ Assets/Scripts/Controllers/MapController.cs
@@ -7,6 +7,29 @@ public class MapController : MonoBehaviour {
     [Header("Map Size")]
     public Vector2 bounds = Vector2.one;
 
+
+    private List<Door> doors;
+
+    private void Awake() {
+        doors = new List<Door>();
+
+        foreach (Door door in GetComponentsInChildren<Door>()) {
+            doors.Add(door);
+        }
+    }
+
+    public void DissableDoors() {
+        foreach (Door door in doors) {
+            door.EnabledInteraction = false;
+        }
+    }
+
+    public void EnableDoors() {
+        foreach (Door door in doors) {
+            door.EnabledInteraction = true;
+        }
+    }
+
     private void OnDrawGizmos() {
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(transform.position, bounds);
