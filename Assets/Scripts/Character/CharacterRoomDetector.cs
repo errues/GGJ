6bc5437c66d4b7d3ecc000c6452d69df7ccc31c2
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2019-01-27 03:13:59 +0100

    OST Controller

---
+++ Assets/Scripts/Character/CharacterRoomDetector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRoomDetector : MonoBehaviour {
+
+    private ExplorationRoom enteredRoom;
+    private OSTController ostController;
+
+    public ExplorationRoom CurrentRoom {
+        get {
+            return enteredRoom;
+        }
+    }
+
+    private void Awake() {
+        ostController = GameObject.FindGameObjectWithTag("MapController").GetComponent<OSTController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        ExplorationRoom room = collision.gameObject.GetComponent<ExplorationRoom>();
+        if (room != null) {
+            enteredRoom = room;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        ExplorationRoom room = collision.gameObject.GetComponent<ExplorationRoom>();
+        if (room != enteredRoom) {
+            if(room.Enlighted) {
+                ostController.PlayLightTheme();
+            } else {
+                ostController.PlayDarkTheme();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/OSTController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSTController : MonoBehaviour {
+
+    public AudioClip lightTheme;
+    public AudioClip darkTheme;
+
+    private bool inFight;
+    private float themeTime;
+    private AudioSource audioSource;
+
+    public bool InFight {
+        get {
+            return inFight;
+        }
+    }
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        inFight = false;
+    }
+
+    public void PlayLightTheme(bool noTime = false) {
+        if(!inFight) {
+            if (noTime) {
+                audioSource.clip = lightTheme;
+                audioSource.Play();
+            }
+            else {
+                themeTime = audioSource.time;
+                audioSource.clip = lightTheme;
+                audioSource.Play();
+                audioSource.time = themeTime;
+            }
+        }
+    }
+
+    public void PlayDarkTheme(bool noTime = false) {
+        if (!inFight) {
+            if (noTime) {
+                audioSource.clip = darkTheme;
+                audioSource.Play();
+            }
+            else {
+                themeTime = audioSource.time;
+                audioSource.clip = darkTheme;
+                audioSource.Play();
+                audioSource.time = themeTime;
+            }
+        }
+    }
+
+    public void PlayFightTheme(AudioClip fightTheme) {
+        inFight = true;
+        audioSource.clip = fightTheme;
+        audioSource.Play();
+    }
+
+    public void FinishFightTheme() {
+        inFight = false;
+        PlayLightTheme(true);
+        audioSource.time = themeTime;
+    }
+}
